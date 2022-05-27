843a490d1c2f6b0ec07b1308389926785130fb73
zhangyf <feng19105306350@163.com>
2022-05-27 16:09:13 +0800

    fix todo

--- src/medium/diff/index.ts
+++ src/medium/diff/index.ts
@@ -1,15 +1,22 @@
 /* _____________ Your Code Here _____________ */
-// TODO 限制O O1为 Record<string, unknown> P无法推断为string
-type Diff<O extends Record<string, unknown>, O1 extends Record<string, unknown>> = {
-  [P in Exclude<keyof O | keyof O1, keyof O & keyof O1>]: P extends keyof O
-    ? O[P]
-    : P extends keyof O1
-    ? O1[P]
+
+type Diff<T, K> = {
+  [P in Exclude<keyof T | keyof K, keyof T & keyof K>]: P extends keyof T
+    ? T[P]
+    : P extends keyof K
+    ? K[P]
     : never
 }
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+]
 
 type Foo = {
   name: string
@@ -24,10 +31,3 @@ type Coo = {
   name: string
   gender: number
 }
-
-type cases = [
-  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
-  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
-  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
-  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
-]
--- src/mod/requiredBykeys.ts
+++ src/mod/requiredBykeys.ts
@@ -3,13 +3,11 @@ type requiredBykeys<T, K extends keyof T> = {
   [P in K]-?: T[P]
 } & Omit<T, K>
 
-// TODO why
-type Simplify<T> = {
-  [P in keyof T]: T[P]
-}
-
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect, Alike } from '@type-challenges/utils'
+
+// TODO Equal Alike
+type Simplify<T> = Pick<T, keyof T>
 
 type cases = [
   Expect<Equal<Simplify<requiredBykeys<User2, 'id'>>, User1>>,
