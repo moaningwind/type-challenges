20a0907255ae46f17a966f18eacde9f7c1c9cc23
zhangyf <feng19105306350@163.com>
2022-06-02 11:24:33 +0800

    perf

--- src/medium/fill/index.ts
+++ src/medium/fill/index.ts
@@ -1,13 +1,13 @@
 /* _____________ Your Code Here _____________ */
-import { Sub } from './../../utils/index';
+import { Sub } from './../../utils/index'
 
 // 这题不是很好写，需要分清楚边界条件，哪里需要 fill 原数组的元素，哪里需要 fill 新的元素
 // 利用好函数参数默认值，以尾递归的形式实现，也可不使用尾递归
 // 不过个人觉得这题使用尾递归的思路更好理解一些，不采用尾递归的话就是由子结果得到最终结果
 
-type gt<Left extends number, Right extends number> = Sub<
-  Left,
-  Right
+type GreaterThanOrEqual<First extends number, Second extends number> = Sub<
+  First,
+  Second
 > extends -1
   ? false
   : true
@@ -21,8 +21,8 @@ type Fill<
   Cur extends any[] = [] // 表示当前的下标
 > = Cur['length'] extends T['length']
   ? []
-  : gt<Cur['length'], Start> extends true
-  ? gt<Cur['length'], End> extends true
+  : GreaterThanOrEqual<Cur['length'], Start> extends true
+  ? GreaterThanOrEqual<Cur['length'], End> extends true
     ? [T[Cur['length']], ...Fill<T, N, Start, End, [...Cur, any]>]
     : [N, ...Fill<T, N, Start, End, [...Cur, any]>]
   : [T[Cur['length']], ...Fill<T, N, Start, End, [...Cur, any]>]
@@ -37,8 +37,8 @@ type FillByTail<
   newArray extends any[] = []
 > = Array['length'] extends T['length']
   ? newArray
-  : gt<Array['length'], Start> extends true
-  ? gt<Array['length'], End> extends true
+  : GreaterThanOrEqual<Array['length'], Start> extends true
+  ? GreaterThanOrEqual<Array['length'], End> extends true
     ? FillByTail<T, N, Start, End, [...Array, any], [...newArray, T[Array['length']]]>
     : FillByTail<T, N, Start, End, [...Array, any], [...newArray, N]>
   : FillByTail<T, N, Start, End, [...Array, any], [...newArray, T[Array['length']]]>
--- src/medium/greaterThan /index.ts
+++ src/medium/greaterThan /index.ts
@@ -1,5 +1,6 @@
 /* _____________ Your Code Here _____________ */
 
+// TODO implement GreaterThanOrEqual type
 type GreaterThan<
   T extends number,
   U extends number,
@@ -13,7 +14,7 @@ type GreaterThan<
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type cases = [
+type cases1 = [
   Expect<Equal<GreaterThan<1, 0>, true>>,
   Expect<Equal<GreaterThan<5, 4>, true>>,
   Expect<Equal<GreaterThan<4, 5>, false>>,
@@ -21,6 +22,6 @@ type cases = [
   Expect<Equal<GreaterThan<20, 20>, false>>,
   Expect<Equal<GreaterThan<10, 100>, false>>,
   Expect<Equal<GreaterThan<111, 11>, true>>,
-  Expect<Equal<GreaterThan<999, 999>, false>>,
+  Expect<Equal<GreaterThan<999, 999>, false>>
   // Expect<Equal<GreaterThan<1000, 1000>, false>>
 ]
